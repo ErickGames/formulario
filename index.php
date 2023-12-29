d7fcca4ad3646099634543072638145133d53eed
Sergio <sergioa.suarez@hotmail.com>
2023-12-28 22:04:25 -0600

    Avance pedorro
    Alch casi ni le movi me cago en todo

--- index.php
+++ index.php
@@ -7,9 +7,23 @@
 </head>
 <body>
 
-    <div class="container">
-        <h1 class="title">Diagnóstico</h1>
-        <p class="description">Este formulario de diagnóstico evaluará aspectos de tu crecimiento como médico.</p>
+<div class="container">
+        <h1 class="title">Bienvenido al Examen de Diagnóstico para Profesionales Médicos</h1>
+        <div class="description">
+            <p>Este formulario está diseñado para brindarte la oportunidad de realizar una autoevaluación en relación con diez aspectos cruciales para tu consultorio o negocio 
+            médico. Tu opinión es fundamental para identificar áreas de mejora y fortaleza en tu práctica profesional.</p>
+
+            <p><b>Modo de Evaluación:</b></p>
+            <p>Utilizaremos una escala del 1 al 5 para evaluar cada aspecto. Siendo la primer respuesta el nivel más bajo en relación a la pregunta planteada, mientras que la última 
+             denota el nivel más alto.</p>
+
+            <p><b>Instrucciones:</b></p>
+            <p>Responde cada pregunta honestamente según tu percepción sobre la situación actual en tu práctica médica. No hay respuestas 
+            correctas o incorrectas, simplemente marca la opción que mejor refleje tu situación.</p>
+
+            <p>Tu participación es invaluable y nos ayudará a ofrecerte información personalizada para mejorar tu desempeño y la calidad de tu servicio.</p>
+
+            <p>¡Gracias por dedicar tiempo a esta autoevaluación!.</div>
         <a href="1_venta_merca.php" class="start-button">Comenzar</a>
     </div>
 
