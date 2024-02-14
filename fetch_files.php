757489d76a98bccbfd63e077c970f95ee250122f
Erick  Diaz <DIAZE25@na.ad.whirlpool.com>
2024-02-14 08:43:57 -0600

    arreglar administrador y fetch_files

--- fetch_files.php
+++ fetch_files.php
@@ -1,9 +1,9 @@
 <?php
 $folderPath = 'PDFS';
 $files = scandir($folderPath);
-$pdfFiles = array_filter($files, function ($file) {
+$pdfFiles = array_values(array_filter($files, function ($file) {
     return pathinfo($file, PATHINFO_EXTENSION) === 'pdf';
-});
+}));
 
 echo json_encode($pdfFiles);
 ?>
